7dc5e78a99091c52f1b69175b0c3c42c769f5191
Kaustubh Kulkarni <57136442+kaustubhk24@users.noreply.github.com>
2022-06-15 08:42:29 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -333,3 +333,15 @@ function onBackKeyDown() {
     //History has at least one previous element: just go back to it:
     navigator.app.backHistory();
 }
+
+
+    let dateDropdown = document.getElementById('batchYear');
+    let currentYear = new Date().getFullYear()+3;
+    let earliestYear = 2010;
+    while (currentYear >= earliestYear) {
+      let dateOption = document.createElement('option');
+      dateOption.text = currentYear;
+      dateOption.value = currentYear;
+      dateDropdown.add(dateOption);
+      currentYear -= 1;
+    }
