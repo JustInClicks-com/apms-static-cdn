2fdcd5164ee3aa90ba51df986897ab06327b6c28
Kaustubh Kulkarni <57136442+kaustubhk24@users.noreply.github.com>
2022-06-26 20:33:43 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -335,13 +335,4 @@ function onBackKeyDown() {
 }
 
 
-    let dateDropdown = document.getElementById('batchYear');
-    let currentYear = new Date().getFullYear()+3;
-    let earliestYear = 2010;
-    while (currentYear >= earliestYear) {
-      let dateOption = document.createElement('option');
-      dateOption.text = currentYear;
-      dateOption.value = currentYear;
-      dateDropdown.add(dateOption);
-      currentYear -= 1;
-    }
+  
